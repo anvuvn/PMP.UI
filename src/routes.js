faa5308865813c57968d4d6793b27f3d861c737e
uyen <uyenbnguyen75@gmail.com>
2024-02-06 10:52:08 -0600

    fix conflict owner page

--- src/routes.js
+++ src/routes.js
@@ -9,14 +9,10 @@ import Users from "./pages/admin/users"
 import AdminDashBoard from "./components/dashboard/admin-dashboard"
 import Error403Permission from "./pages/403"
 import MessageList from "./pages/messages/message-list"
-import OwnerDashBoard from "./components/dashboard/owner-offersList"
-import OwnerProperty from "./pages/owner/ower-properties"
-<<<<<<< HEAD
-import AdminProperties from "./pages/admin/admin-properties"
 import OwnerOffersList from "./components/dashboard/owner-offersList"
-=======
+import OwnerProperty from "./pages/owner/ower-properties"
+import AdminProperties from "./pages/admin/admin-properties"
 import OfferList from "./pages/owner/offerList"
->>>>>>> 9d93da529c1534777abaf347e3b48f243b80b578
 
 const PageRoutes = () => {
     return <Routes>
@@ -34,11 +30,7 @@ const PageRoutes = () => {
 
         </Route>
         <Route path="/owner" element={<OwnerLandingPage></OwnerLandingPage>}>
-<<<<<<< HEAD
             <Route path="offerslist" element={<OwnerOffersList></OwnerOffersList>}></Route>
-=======
-          {/*   <Route path="dashboard" element={<OwnerDashBoard></OwnerDashBoard>}></Route> */}
->>>>>>> 9d93da529c1534777abaf347e3b48f243b80b578
             <Route path="properties" element={<OwnerProperty></OwnerProperty>}></Route>
             <Route path="offerlist" element={<OfferList></OfferList>}></Route>
         </Route>
