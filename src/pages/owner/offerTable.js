1d9a196b8facbd9b5d546c52930367d15ccd3fe3
alexchang <long.chang@live.com>
2024-02-08 12:20:45 -0600

    Add offer list and Actions

--- src/pages/owner/offerTable.js
+++ src/pages/owner/offerTable.js
@@ -1,17 +1,42 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Dropdown from 'react-bootstrap/Dropdown';
 import OwnerService from "../../service/owner-service";
-import {Link} from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import MessageDialog from '../../components/messages/message-dialog';
+import { OfferStatus } from '../../constant/OfferStatus';
 
-const OfferTable = ({ data, action }) => {
+const OfferTable = ({ action, onDataRefresh }) => {
+
+    const { id } = useParams();
+
+    const [offers, setOffers] = useState([]);
+
+    const fetchOffersByPropertyID = (pid) => {
+
+        OwnerService.getOfferByProperty(pid).then(
+            response => {
+                setOffers(response);
+            }
+        ).catch(err => console.log(err.message));
+
+    }
+
+    console.log("offersList:", offers);
+
+    useEffect(() => {
+
+        console.log("passs property ID:", id);
+
+        fetchOffersByPropertyID(id);
+
+    }, [id]);
 
     const [ownerId, setOwnerId] = useState();//Need to ask Anh
 
     const [selectedStatus, setSelectedStatus] = useState(null);
 
     const statusHandleChange = (event) => {
-
 
         setSelectedStatus(event);
 
@@ -21,16 +46,15 @@ const OfferTable = ({ data, action }) => {
     //Need to handle get offers by status 
     const fetchOffersByStatus = (status) => {
 
-        alert("Status:::" + status);
-
-
+        OwnerService.getOfferByStatus(status).then(res => {
+            setOffers(res);
+        })
     }
-
+    const reloadData = () => {
+        if (typeof onDataRefresh === "function")
+            onDataRefresh();
+    }
     const acceptHandleChange = (d) => {
-
-        alert("d.id" + d.id);
-
-
         const data = {
             offerId: d.id
         }
@@ -39,13 +63,13 @@ const OfferTable = ({ data, action }) => {
         if (result) {
             OwnerService.acceptOffer(d.id, data).then(response => {
                 console.log(response);
+                alert("Offer Accepted")
+                reloadData()
             }).catch(error => console.log(error.message));
         }
     };
 
     const declineHandleChange = (d) => {
-
-        setOwnerId(d.property.ownerId);
 
         const data = {
             offerId: d.id
@@ -57,17 +81,26 @@ const OfferTable = ({ data, action }) => {
             OwnerService.cancelOffer(d.id, data)
                 .then(res => {
                     console.log(res);
+                    alert("Offer cancelled")
+                    reloadData()
                 }).catch(error => console.log(error.message));
 
         }
     }
 
-    const getAction=()=>{
+    const getAction = () => {
 
         alert("Send Message::");
     };
 
-
+    const getMessageIcon = () => {
+        return <button type='button' class="btn-message btn btn-outline-warning" btn-sm style={{ marginLeft: 10 }}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-arrow-up" viewBox="0 0 16 16">
+                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v4.5a.5.5 0 0 1-1 0V5.383l-7 4.2-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h5.5a.5.5 0 0 1 0 1H2a2 2 0 0 1-2-1.99zm1 7.105 4.708-2.897L1 5.383zM1 4v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1" />
+                <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.354-5.354 1.25 1.25a.5.5 0 0 1-.708.708L13 12.207V14a.5.5 0 0 1-1 0v-1.717l-.28.305a.5.5 0 0 1-.737-.676l1.149-1.25a.5.5 0 0 1 .722-.016" />
+            </svg>
+        </button>
+    }
     return (
         <div>
             <h1>Customer Offer List</h1>
@@ -87,13 +120,13 @@ const OfferTable = ({ data, action }) => {
                             </Dropdown.Toggle>
                             <Dropdown.Menu>
 
-                                <Dropdown.Item key="created" eventKey="created">created</Dropdown.Item>
-                                <Dropdown.Item key="pending" eventKey="pending">pending</Dropdown.Item>
-                                <Dropdown.Item key="contingent" eventKey="Contingent">Contingent</Dropdown.Item>
-                                <Dropdown.Item key="OwnerAccepted" eventKey="OwnerAccepted">Owner Accepted</Dropdown.Item>
-                                <Dropdown.Item key="CustomerAccepted" eventKey="CustomerAccepted">Customer Accepted</Dropdown.Item>
-                                <Dropdown.Item key="cancelled" eventKey="cancelled">cancelled</Dropdown.Item>
-                                <Dropdown.Item key="rejected" eventKey="rejected">rejected</Dropdown.Item>
+                                <Dropdown.Item key={OfferStatus.created} eventKey={OfferStatus.created}>created</Dropdown.Item>
+                                <Dropdown.Item key={OfferStatus.pending} eventKey={OfferStatus.pending}>pending</Dropdown.Item>
+                                <Dropdown.Item key={OfferStatus.contingent} eventKey={OfferStatus.contingent}>Contingent</Dropdown.Item>
+                                <Dropdown.Item key={OfferStatus.OwnerAccepted} eventKey={OfferStatus.OwnerAccepted}>Owner Accepted</Dropdown.Item>
+                                <Dropdown.Item key={OfferStatus.CustomerAccepted} eventKey={OfferStatus.CustomerAccepted}>Customer Accepted</Dropdown.Item>
+                                <Dropdown.Item key={OfferStatus.cancelled} eventKey={OfferStatus.cancelled}>cancelled</Dropdown.Item>
+                                <Dropdown.Item key={OfferStatus.rejected} eventKey={OfferStatus.rejected}>rejected</Dropdown.Item>
 
                             </Dropdown.Menu>
                         </Dropdown>
@@ -104,44 +137,40 @@ const OfferTable = ({ data, action }) => {
                 <div className="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
                     <table className="min-w-full divide-y divide-gray-200">
                         <thead className="bg-gray-50">
-                        <tr>
-                            <th scope="col"
-                                className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">CUSTOMER
-                            </th>
-                            <th scope="col"
-                                className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">OFFER
-                                PRICE
-                            </th>
-                            <th scope="col"
-                                className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">STATUS
-                            </th>
-                            <th scope="col"
-                                className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">ACTION
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data ? data.map((d, index) => {
-                            return <tr key={d.id}>
-                                {/*  <td>{d.id}</td> */}
-                                <td>{d.customer.name}</td>
-                                <td>{d.amount}</td>
-                                <td>{d.status}</td>
-
-                                <td>
-                                    <Button variant="outline-success" style={{ marginRight: 10 }} onClick={() => acceptHandleChange(d)}>Accept</Button>{' '}
-                                    <Button variant="outline-danger" onClick={() => declineHandleChange(d)}>Decline</Button>{' '}
-                                    <button type='button' class="btn-message btn btn-outline-warning" btn-sm style={{ marginLeft: 10 }} onClick={getAction}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-arrow-up" viewBox="0 0 16 16">
-                                            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v4.5a.5.5 0 0 1-1 0V5.383l-7 4.2-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h5.5a.5.5 0 0 1 0 1H2a2 2 0 0 1-2-1.99zm1 7.105 4.708-2.897L1 5.383zM1 4v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1" />
-                                            <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.354-5.354 1.25 1.25a.5.5 0 0 1-.708.708L13 12.207V14a.5.5 0 0 1-1 0v-1.717l-.28.305a.5.5 0 0 1-.737-.676l1.149-1.25a.5.5 0 0 1 .722-.016" />
-                                        </svg>
-                                    </button>
-                                </td>
-
-
+                            <tr>
+                                <th scope="col"
+                                    className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">CUSTOMER
+                                </th>
+                                <th scope="col"
+                                    className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">OFFER
+                                    PRICE
+                                </th>
+                                <th scope="col"
+                                    className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">STATUS
+                                </th>
+                                <th scope="col"
+                                    className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">ACTION
+                                </th>
                             </tr>
-                        )}
+                        </thead>
+                        <tbody>
+                            {offers ? offers.map((d, index) => {
+                                return <tr key={d.id}>
+                                    {/*  <td>{d.id}</td> */}
+                                    <td>{d.name}</td>
+                                    <td>{d.amount}</td>
+                                    <td>{d.status}</td>
+
+                                    <td>
+                                        <Button variant="outline-success" style={{ marginRight: 10 }} onClick={() => acceptHandleChange(d)}>Accept</Button>{' '}
+                                        <Button variant="outline-danger" onClick={() => declineHandleChange(d)}>Decline</Button>{' '}
+
+                                        <MessageDialog title={getMessageIcon()} receiver={{ id: d.customer_id, name: d.name }}></MessageDialog>
+                                    </td>
+
+
+                                </tr>
+                            }) : ""}
                         </tbody>
                     </table>
                 </div>
