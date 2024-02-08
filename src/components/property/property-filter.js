49aa0716cb2a84526f54b15b9e260de312f7c6a2
alexchang <long.chang@live.com>
2024-02-08 16:53:19 -0600

    add default filter and allow accept offer

--- src/components/property/property-filter.js
+++ src/components/property/property-filter.js
@@ -14,7 +14,7 @@ const PropertyFilter = ({ onSearch }) => {
             maxPrice: parseFloat(form["max"].value) || 0,
             propertyType: form["propertyType"].value,
             //location: form["location"].value,
-            numberOfRoom: parseFloat(form["numberOfRoom"].value) || 1
+            numberOfRoom: parseFloat(form["numberOfRoom"].value)
         }
         console.log(searchInfo)
         PropertyService.searchProperty(searchInfo).then(res => {
@@ -34,13 +34,14 @@ const PropertyFilter = ({ onSearch }) => {
                 <form ref={formRef} className={"flex justify-content-evenly items-center space-x-4"}>
                     <input
                         className="flex-grow h-12 px-2 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500"
-                        placeholder="Min" name="min" type="number"/>
+                        placeholder="Min" name="min" type="number" />
                     <input
                         className="flex-grow h-12 px-2 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500"
-                        type="number" placeholder="Max" name="max"/>
+                        type="number" placeholder="Max" name="max" />
 
                     <select name="propertyType"
-                            className="flex-grow h-12 px-2 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500">
+                        className="flex-grow h-12 px-2 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500">
+                        <option value={""} key="all">All</option>
                         {Object.keys(PropertyType).map(k => {
                             return <option value={k} key={k}>{PropertyType[k]}</option>
                         })}
@@ -49,14 +50,19 @@ const PropertyFilter = ({ onSearch }) => {
                         className="flex-grow h-12 px-2 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500"
                         name="numberOfRoom">
                         {
-                            Array.from({length: 10}).map((o, i) => {
-                                return <option value={i + 1} key={i}> {i + 1}</option>
-                            })
+                            <>
+                                <option value={0} key={0}>All</option>
+                                {
+                                    Array.from({ length: 10 }).map((o, i) => {
+                                        return <option value={i + 1} key={i}> {i + 1}</option>
+                                    })
+                                }
+                            </>
                         }
                     </select>
                 </form>
                 <button className="flex-grow h-12 px-2 rounded-lg bg-indigo-600 text-white hover:bg-indigo-500"
-                        onClick={applySearch}>Apply
+                    onClick={applySearch}>Apply
                 </button>
             </div>
         </div>
